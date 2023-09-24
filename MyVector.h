7647cb72a2bf2acec3e36cf48fee98f4059cd3c7
ZkunCry <aksentev.03@mail.ru>
2023-09-24 13:38:21 +0500

    added erase , insert, find methods

--- MyVector.h
+++ MyVector.h
@@ -22,7 +22,15 @@ public:
 	Vector(const Vector<_T> &vect);
 	Vector(size_t count, const_pointer arr);
 	void push_back(const_reference value);
-	~Vector();
+	int find(const _T& value)const;
+	void erase(const _T& position);
+	void insert(const _T& value, const size_t pos);
+	const size_t Size()const;
+	_T& operator[](int index)const;
+	~Vector()noexcept;
+	_T* begin();
+	_T* end();
+
 };
 
 template<class _T>
@@ -38,7 +46,7 @@ inline void Vector<_T>::ReallocMem()
 	Vector buff(*this);
 	delete[]Target;
 	Target = new _T[Capacity];
-	
+	std::copy_n(buff.Target, buff.size, Target);
 }
 
 template<class _T>
@@ -93,11 +101,79 @@ inline void Vector<_T>::push_back(const_reference value)
 }
 
 template<class _T>
-inline Vector<_T>::~Vector()
+inline  int  Vector<_T>::find(const _T& value) const
+{
+	int first = 0;
+	int last = size -1;
+	while (first <= last) {
+		int mid = (first + last) / 2;
+		if (Target[mid] == value)
+			return mid;
+		else if (Target[mid] < value)
+			first = mid + 1;
+		else if (Target[mid] > value)
+			last = mid - 1;
+	}
+	return -1;
+}
+
+template<class _T>
+inline void Vector<_T>::erase(const _T& position)
+{	
+	auto positionSwap = position - 1;
+	auto swapElem = Target[position];
+	std::copy_n(this->Target, positionSwap-1, this->Target);
+	std::copy_n(this->Target + positionSwap +1, positionSwap + 1, this->Target+ positionSwap);
+	size--;
+
+}
+
+template<class _T>
+inline void Vector<_T>::insert(const _T& value, const size_t pos)
+{
+	auto positionInsert = pos - 1;
+	if (pos > size || pos < 0)
+		return;
+	if (pos > Capacity ||size > Capacity) {
+		Resizeable();
+		ReallocMem();	
+	}
+
+	size++;
+	std::copy_n(this->Target + positionInsert, (size - positionInsert)-1, Target + positionInsert + 1);
+	Target[positionInsert] = value;
+	
+}
+
+template<class _T>
+inline const size_t Vector<_T>::Size() const
+{
+	return this->size;
+}
+
+template<class _T>
+inline _T& Vector<_T>::operator[](int index) const
+{
+	return *(this->Target + index);
+}
+
+template<class _T>
+inline Vector<_T>::~Vector()noexcept
 {
 	delete[]Target;
 	Target = nullptr;
 	Capacity = NULL;
 	size = NULL;
+}
 
+template<class _T>
+inline _T* Vector<_T>::begin()
+{
+	return this->Target;
+}
+
+template<class _T>
+inline _T* Vector<_T>::end()
+{
+	return this->Target+size;
 }
--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 ﻿
 #include <iostream>
 #include "MyVector.h"
+#include <vector>
+#include <algorithm>
 
 int main()
 {
 	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	std::vector<int> vec2;
 
 	Vector<int> vec(10,arr);
-	vec.push_back(999);
-	vec.push_back(9991);
+	for (auto value : vec)
+		std::cout << value << std::endl;
+	std::cout<<vec.find(10);
 }
